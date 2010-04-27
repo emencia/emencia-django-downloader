e813d9c4c2803f92e3608ed4d21e647329e70d6e
Lafaye Philippe <lafaye@emencia.com>
2010-04-27 16:18:48 +0200

    delete ajax view

--- emencia/django/downloader/urls.py
+++ emencia/django/downloader/urls.py
@@ -2,11 +2,9 @@
 """Urls for emencia.django.downloader"""
 from django.conf.urls.defaults import *
 
-from emencia.django.downloader.views import get_file, upload_progress,\
-                                            upload,upload_ok
+from emencia.django.downloader.views import get_file, upload, upload_ok
 
 urlpatterns = patterns('',
-                       url(r'^upload/progress/$', upload_progress, name='upload_progress'),
                        url(r'^upload/(?P<slug>[-\w]+)/$', upload_ok, name='upload_ok'),
                        url(r'^(?P<slug>[-\w]+)/$', get_file, name='get_file'),
                        url(r'^$', upload, name='upload'),
